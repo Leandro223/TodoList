84bb686b40a0eb8690ae3af11af4b8f696b3c113
Baracho <baracholss@gmail.com>
2023-02-04 15:27:26 -0300

    armazenamento no navegador

--- src/app/modules/home/componets/todo-list/todo-list.component.ts
+++ src/app/modules/home/componets/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 
 @Component({
@@ -6,15 +6,20 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements OnInit, DoCheck {
 
-  public taskList: Array<TaskList> = [
+  /*public taskList: Array<TaskList> = [
     {task: "Minha nova Task", checked: false},
     {task: "Minha nova Task2", checked: true}
-  ];
+  ];*/
+
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
 
   
   constructor() { }
+  ngDoCheck(): void {
+    this.setLocalStorage();
+  }
 
   ngOnInit(): void {
   }
@@ -40,6 +45,13 @@ export class TodoListComponent implements OnInit {
       if(confirm){
         this.deleteItemTaskList(index);
       }
+    }
+  }
+
+  public setLocalStorage(){
+    if(this.taskList){
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
     }
   }
 
