4aef3ccedfaec05d1c282c0bf12bae43be0b688e
Baracho <baracholss@gmail.com>
2023-02-04 15:18:04 -0300

    implementado metodo de validação de vazio no input

--- src/app/modules/home/componets/todo-list/todo-list.component.ts
+++ src/app/modules/home/componets/todo-list/todo-list.component.ts
@@ -34,4 +34,13 @@ export class TodoListComponent implements OnInit {
     }
   }
 
+  public validationInput(event: string,index: number){
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja deletar?");
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
+    }
+  }
+
 }
